4ec0493d4b1656a436c29e6f445e5d6ef4f5cd3b
NezrinMemmedovaCode <163848940+NezrinMemmedovaCode@users.noreply.github.com>
2024-07-07 13:53:49 +0400

    add route

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,7 +1,6 @@
-
-import './App.css'
+import './App.css';
 import { HelmetProvider } from 'react-helmet-async';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainLayout from './Layout/MainLayout';
 import Home from './pages/home/Home';
 import Basket from './pages/basket/Basket';
@@ -9,33 +8,34 @@ import Wish from './pages/wish/Wish';
 import Add from './pages/add/Add';
 import Admin from './pages/admin/Admin';
 import Contact from './pages/contact/Contact';
-import MainProvider from './context/MainProvider';
+import Register from './pages/auth/register/Register';
+import Login from './pages/auth/login/Login';
 import CoreProvider from './context/CoreProvider';
+import PrivateRoute from './components/PrivateRoute'; 
 
 function App() {
-
   return (
     <>
-    <CoreProvider>
-     <HelmetProvider>
-
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="Basket" element={<Basket />} />
-          <Route path="Wish" element={<Wish />} />
-          <Route path="Add" element={<Add />} />
-          <Route path="Admin" element={<Admin />} />
-          <Route path="Contact" element={<Contact />} />
-           </Route>
-      </Routes>
-    </BrowserRouter>
-     </HelmetProvider>
-     </CoreProvider>
-      
+      <CoreProvider>
+        <HelmetProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<MainLayout />}>
+                <Route index element={<Home />} />
+                <Route path="Basket" element={<PrivateRoute element={<Basket />} />} />
+                <Route path="Wish" element={<PrivateRoute element={<Wish />} />} />
+                <Route path="Add" element={<PrivateRoute element={<Add />} />} />
+                <Route path="Admin" element={<PrivateRoute element={<Admin />} />} />
+                <Route path="Contact" element={<PrivateRoute element={<Contact />} />} />
+                <Route path="Register" element={<Register />} />
+                <Route path="Login" element={<Login />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </HelmetProvider>
+      </CoreProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
